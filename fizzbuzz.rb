fc7ed1263bdccf48ce0aaeb79744262d9c090405
Cheng-Yu Hsu <m@cyhsu.me>
2016-09-21 10:18:10 +0800

    Finish :tada:

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,23 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+
+# frozen_string_literal: true
+
+def divided_by?(divisor)
+  -> (dividend) { (dividend % divisor).zero? }
+end
+
+def fizzbuzzify(turn)
+  case turn
+  when divided_by?(15) then 'FizzBuzz'
+  when divided_by?(5)  then 'Buzz'
+  when divided_by?(3)  then 'Fizz'
+  else turn
+  end
+end
+
+def fizzbuzz(count, &block)
+  sequence = (1..count).map { |turn| fizzbuzzify(turn) }
+  sequence.each(&block) if block
+  sequence
+end
