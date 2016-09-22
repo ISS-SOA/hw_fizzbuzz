d893d783a27f07cb2727ef4068fbbb38b8e61f01
Soumya Ray <soumya.ray@gmail.com>
2016-09-22 15:46:34 +0800

    Freeze constants in specs

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -3,7 +3,7 @@ require './fizzbuzz'
 
 FIZZBUZZ_30 = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11,
                'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz',
-               22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz']
+               22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz'].freeze
 
 describe 'Fizz buzz tests' do
   it 'should return an array of numbers and strings' do
