98c45a35f75ce7b5cc71aa4c0184c91c70dfd67e
Soumya Ray <soumya.ray@gmail.com>
2022-09-25 14:53:58 +0800

    Add Fizz Buzz rules to tests

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'minitest/rg'
 require './fizzbuzz'
 
 FIZZBUZZ = %w[
@@ -8,7 +9,37 @@ FIZZBUZZ = %w[
   Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz
 ].freeze
 
-describe 'Fizz buzz tests' do
+describe 'Rules of Fizz Buzz' do
+  large_n = 15_000
+  result = fizzbuzz(large_n)
+  rand_int = Random.srand % 1000
+
+  it 'should recognize multiples of 3' do
+    rand3 = rand_int - (rand_int % 3)
+    word = result[rand3 - 1]
+    _(word).must_include 'Fizz'
+  end
+
+  it 'should recognize multiples of 5' do
+    rand5 = rand_int - (rand_int % 5)
+    word = result[rand5 - 1]
+    _(word).must_include 'Buzz'
+  end
+
+  it 'should recognize multiples of 15' do
+    rand15 = rand_int - (rand_int % 15)
+    word = result[rand15 - 1]
+    _(word).must_equal 'FizzBuzz'
+  end
+
+  it 'should recognized numbers not multiples of 3, 5, 15' do
+    rand_not15 = rand_int - (rand_int % 15) + 1
+    word = result[rand_not15 - 1]
+    _(word.to_i).must_equal rand_not15
+  end
+end
+
+describe 'Correct return types and blocks' do
   it 'should return an array of strings' do
     _(fizzbuzz(30)).must_equal FIZZBUZZ
   end
